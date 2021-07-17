b5ac4904617ac338b6543fe12da2563f7b6926d6
name <varunbrahmane30@gmail.com>
2021-07-17 15:44:50 +0530

    [varun] added: welcome-line computition using oops

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LineComputitionProblemUsingOOPs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Computition Problem Using OOPs");
+            Console.ReadKey();
+        }
+    }
+}
