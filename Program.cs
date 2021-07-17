e86906bb5f78c8d376f4ff9213e3415ef5f9fffa
name <varunbrahmane30@gmail.com>
2021-07-17 16:54:23 +0530

    [varun] added: using methods and creating object

--- Program.cs
+++ Program.cs
@@ -9,7 +9,19 @@ namespace LineComputitionProblemUsingOOPs
             Console.WriteLine("Welcome to Line Computition Problem Using OOPs");
             //LIneLength.getCartesianPoint();
             //LinesEqualOrNot.checkEquality();
-            CheckEqualGreterLesser.checkEqulGreatLess();
+            //CheckEqualGreterLesser.checkEqulGreatLess();
+
+            #region UC4- using oops Operation
+
+            // object creation of class 
+            UsingOOPsOperation op = new UsingOOPsOperation();
+
+            // calling methods of class (UsingOOPsOperation)
+            op.getValue();
+            op.cal();
+            op.result();
+            #endregion
+
             Console.ReadKey();
         }
     }
